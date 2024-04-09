50fbf940d01a2983171912091a87918dd76400d1
Alejandro Rodríguez Vallín <162721455+alejandroRV05@users.noreply.github.com>
2024-04-09 15:31:03 +0200

    Add files via upload

---
+++ Pantalla_Inicio.java
@@ -0,0 +1,28 @@
+package Pandemic.Clases;
+
+public class Pantalla_Inicio {
+
+	
+	//Funciones
+	
+	
+	//Es posible que algunas de estas funciones usen Java Swing
+	
+	public void cargarPantallaInicio() {
+		
+	}
+	
+	
+	public void cargarRecords() {
+		
+	}
+	
+	public void cargarPantallaGuardado() {
+		
+	}
+	
+	
+	public void cargarPartida() {
+		
+	}
+}
